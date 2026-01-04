7e65bbbd81b9a70ba7c2fb895763111d4f3e33d0
kelvine99 <kipkelvin99@gmail.com>
2026-01-04 22:50:23 +0300

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
+from.models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    # context_object_name = "blogs"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name= 'post_detail.html'
+
+# Create your views here.
